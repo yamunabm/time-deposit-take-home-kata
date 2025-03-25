8557730d9a4c147bc3c21d9f93fdea54e9b521f2
theochiu2010 <alanchiu3730@gmail.com>
2025-03-25 19:41:12 +0800

    fix typescript method name

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,5 +3,5 @@ import { TimeDepositCalculator } from './TimeDepositCalculator'
 
 const calc = new TimeDepositCalculator()
 const plans: TimeDeposit[] = [new TimeDeposit('basic', 1234567.0, 45)]
-const interest = calc.calculateInterest(plans)
+const interest = calc.updateBalance(plans)
 console.log({ interest })
